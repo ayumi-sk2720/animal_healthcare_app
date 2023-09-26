40b8432aad7a8878f7af31d0e109de80ddf2afe5
Ayumi Sakauchi <ayumi.sakauchi@sorich.jp>
2023-09-26 14:20:55 +0900

    routerを使用してざっくりページ作成

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <commonHeader></commonHeader>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+import commonHeader from './components/commonHeader'
+  export default {
+    components: {
+      commonHeader
+    },
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ vue/src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="home">
+        <div>
+            <h1>{{ msg }}</h1>
+        </div>
+        <div>
+            <p v-if="msg.length > 0">
+                {{ msg }}
+            </p>
+            <p v-else>
+                no text
+            </p>
+            <input type="text" v-model="msg">
+            <button @click="clear()">clear</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+            msg: 'This is the top page.'
+            }
+        },
+        methods: {
+            clear () {
+            this.msg = '';
+            }
+        }
+    }
+</script>
