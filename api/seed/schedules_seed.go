8bab0641a60b14a5fa596442ca178f8726c50987
myantyuWorld <nepinepi984@gmail.com>
2023-10-19 18:56:34 +0900

    scheduleのseeder追加

---
+++ api/seed/schedules_seed.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	dbconnect "github.com/myantyuWorld/animal_healthcate/database"
+)
+
+type Schedules struct {
+	PetId     int8
+	Title     string
+	Date      time.Time
+	Location  string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+func seeds(db *gorm.DB) error {
+	// TODO: 日本語を投入しようとすると以下警告
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 5; j++ {
+			schedule := Schedules{
+				PetId:     1,
+				Title:     "サンプル" + strconv.Itoa(i),
+				Date:      time.Now(),
+				Location:  "サンプルペットショップ" + strconv.Itoa(j),
+				CreatedAt: time.Now(),
+				UpdatedAt: time.Now(),
+			}
+			if err := db.Create(&schedule).Error; err != nil {
+				fmt.Printf("%+v", err)
+			}
+		}
+	}
+
+	return nil
+}
+
+func main() {
+	// TODO: database.connect.goのConnect()でできるので上記メソッドがいらない
+	// db := dbconnect.Connect()
+	db := dbconnect.Connect()
+	if err := seeds(db); err != nil {
+		fmt.Printf("%+v", err)
+		return
+	}
+	defer db.Close()
+}
