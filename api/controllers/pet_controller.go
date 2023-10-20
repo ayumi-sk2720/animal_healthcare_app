04366be5ef84ce3ec93ce97ab4be76ae4499cccc
myantyuWorld <nepinepi984@gmail.com>
2023-10-20 18:08:19 +0900

    add middleware（リクエスト、レスポンスをログ出力）

--- api/controllers/pet_controller.go
+++ api/controllers/pet_controller.go
@@ -27,6 +27,45 @@ func FetchPet() echo.HandlerFunc {
 		// TODO: 将来的には、テーブル結合した結果を返却する
 		result := db.First(&pet, id)
 		fmt.Printf("pet:: %v\n", &pet)
+		// 実際にはこのJSONを返す必要がある
+		// {
+		// 	"baseInfo": {
+		// 		"name": "チャイ",
+		// 		"age": "3歳8ヶ月",
+		// 		"sex": "メス",
+		// 		"birthday": "2020年1月27日"
+		// 	},
+		// 	"now_wight": {
+		// 		"weight": "4.8kg",
+		// 		"date": "2023年9月13日 10:00"
+		// 	},
+		// 	"target_wight": {
+		// 		"weight": "5.2kg"
+		// 	},
+		// 	"dosage_schedule": {
+		// 		"today": "なし",
+		// 		"next": {
+		// 			"name": "ネクスがーど",
+		// 			"date": "2023年9月15日 10:00"
+		// 		}
+		// 	},
+		// 	"physical_condition": {
+		// 		"food": 3,
+		// 		"sweat": 2,
+		// 		"toilet": 1
+		// 	},
+		// 	"memo": "今月に入って飲む水の量が\\n増えた気がする\\n次に通院した時に先生に相談する",
+		// 	"schedules": [
+		// 		{
+		// 			"date": "2023年9月20日 10:00",
+		// 			"name": "トリミング"
+		// 		},
+		// 		{
+		// 			"date": "2023年9月30日 14:00",
+		// 			"name": "通院"
+		// 		}
+		// 	]
+		// }
 
 		if result.RecordNotFound() {
 			fmt.Printf("レコードが見つかりません")
@@ -60,5 +99,26 @@ func GetShedules() echo.HandlerFunc {
 
 		// db操作
 		return c.JSON(http.StatusOK, &schedules)
+	}
+}
+
+// [example]
+//
+//	{
+//			"title": "サンプル0",
+//			"date": "2023-10-19T18:58:18+09:00",
+//			"location": "サンプルペットショップ0"
+//	}
+func PostSchedule() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		// Binding | https://echo.labstack.com/docs/binding
+		schedule := new(model.PostSchedule)
+		if error := c.Bind(&schedule); error != nil {
+			fmt.Println()
+			return c.JSON(http.StatusBadRequest, "bad request")
+		}
+		fmt.Println(schedule)
+
+		return c.JSON(http.StatusOK, &schedule)
 	}
 }
--- api/router.go
+++ api/router.go
@@ -1,23 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+
 	// controller "api/controllers"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	"github.com/myantyuWorld/animal_healthcate/controllers"
 )
 
+func bodyDumpHandler(c echo.Context, reqBody, resBody []byte) {
+	fmt.Printf("Request Body: %v\n", string(reqBody))
+	fmt.Printf("Response Body: %v\n", string(resBody))
+	fmt.Println("==================================================================")
+}
+
 func newRouter() *echo.Echo {
 	e := echo.New()
 
 	e.Use(middleware.CORS())
+	e.Use(middleware.BodyDump(bodyDumpHandler))
 
 	// ルーティング
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, golang echo!")
 	})
 	e.GET("pet/:id", controllers.FetchPet())
-	e.GET("pet/:id/schedule", controllers.GetShedules())
+	e.GET("pet/:id/schedules", controllers.GetShedules())
+	e.POST("pet/:id/schedules", controllers.PostSchedule())
+
 	return e
 }
