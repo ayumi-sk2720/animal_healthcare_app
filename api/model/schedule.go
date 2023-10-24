be90e0d96bcf9d79859af89b922f30cac6828183
myantyuWorld <nepinepi984@gmail.com>
2023-10-24 09:50:46 +0900

    modify swagger

--- api/model/schedule.go
+++ api/model/schedule.go
@@ -3,11 +3,9 @@ package model
 import "time"
 
 type Schedule struct {
-	Id        int       `gorm:"primary_key" json:"id"`
-	PetId     int       `json:"pet_id"`
-	Title     string    `json:"title"`
-	Date      time.Time `json:"date"`
-	Location  string    `json:"location"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	Id       int       `gorm:"primary_key" json:"id"`
+	PetId    int       `json:"pet_id"`
+	Title    string    `json:"title"`
+	Date     time.Time `json:"date"`
+	Location string    `json:"location"`
 }
