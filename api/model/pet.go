1f03f880f4b43ee11c851d5977c98da80175d821
myantyuWorld <nepinepi984@gmail.com>
2023-10-24 09:50:46 +0900

    modify swagger

--- api/model/pet.go
+++ api/model/pet.go
@@ -1,13 +1,12 @@
 package model
 
-import "time"
-
 // User struct -  basic user model
 type Pet struct {
-	Id        int       `gorm:"primary_key" json:"id"`
-	Name      string    `json:"name"`
-	BirthDay  time.Time `json:"birth_day"`
-	Sex       int       `json:"sex"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	Id           int    `gorm:"primary_key" json:"id"`
+	Name         string `json:"name"`
+	Age          string `json:"age"`
+	Sex          string `json:"sex"`
+	NowWeight    string `json:"now_weight"`
+	TargetWeight string `json:"target_weight"`
+	Birthday     string `json:"birthday"`
 }
