3b9a37dc3ac845ca2797b1b8a0f9ed00d3327777
myantyuWorld <nepinepi984@gmail.com>
2023-10-24 15:29:30 +0900

    modify router

--- api/main.go
+++ api/main.go
@@ -1,15 +1,6 @@
 package main
 
-import (
-	"net/http"
-
-	"github.com/labstack/echo"
-)
-
 func main() {
-	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, world!")
-	})
-	e.Logger.Fatal(e.Start(":8080"))
+	router := newRouter()
+	router.Logger.Fatal(router.Start(":8080"))
 }
