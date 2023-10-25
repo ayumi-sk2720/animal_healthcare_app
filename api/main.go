44973345040da1a729c9bcabd9dd6b6e3d7aef68
myantyuWorld <nepinepi984@gmail.com>
2023-10-25 09:49:59 +0900

    add middleware

--- api/main.go
+++ api/main.go
@@ -10,8 +10,6 @@ import (
 
 // Go言語：文法基礎まとめ | https://qiita.com/HiromuMasuda0228/items/65b9a593275f769f6b69
 func main() {
-	// router := newRouter()
-	// router.Logger.Fatal(router.Start(":8080"))
 	fmt.Println("sever start")
 	petHandler := injector.InjectPetHandler()
 	e := echo.New()
