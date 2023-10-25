ea51599da921f80c7efd811416defafa20fa5ad3
myantyuWorld <nepinepi984@gmail.com>
2023-10-25 09:56:42 +0900

    refactor

--- api/database/connect.go
+++ api/database/connect.go
@@ -7,6 +7,7 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
+// TODO: seedで使っているが、sqlhandlerを使うように変更したい
 func Connect() *gorm.DB {
 	dsn := "root:hogehoge@tcp(db:3306)/dev?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open("mysql", dsn)
--- api/infra/sqlhandler.go
+++ api/infra/sqlhandler.go
@@ -1,6 +1,8 @@
 package infra
 
 import (
+	"log"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
@@ -13,6 +15,7 @@ func NewSqlHandler() *SqlHandler {
 	dsn := "root:hogehoge@tcp(db:3306)/dev?charset=utf8mb4&parseTime=True&loc=Local"
 	conn, err := gorm.Open("mysql", dsn)
 	if err != nil {
+		log.Fatalf("Couldn't establish database connection: %s", err)
 		panic(err.Error)
 	}
 	sqlHandler := new(SqlHandler)
