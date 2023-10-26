402cade512f477e2a55c4ab14228d3c69d98441c
myantyuWorld <nepinepi984@gmail.com>
2023-10-26 17:55:04 +0900

    dbの接続情報の渡し方を変更

--- api/database/connect.go
+++ api/database/connect.go
@@ -1,7 +1,9 @@
 package dbconnect
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
@@ -9,7 +11,12 @@ import (
 
 // TODO: seedで使っているが、sqlhandlerを使うように変更したい
 func Connect() *gorm.DB {
-	dsn := "root:hogehoge@tcp(db:3306)/dev?charset=utf8mb4&parseTime=True&loc=Local"
+	user := os.Getenv("MYSQL_USER")
+	pass := os.Getenv("MYSQL_PASSWORD")
+	host := os.Getenv("MYSQL_HOST") // ココ!!
+	dbname := os.Getenv("MYSQL_DATABASE")
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local", user, pass, host, dbname)
+
 	db, err := gorm.Open("mysql", dsn)
 	if err != nil {
 		log.Fatalf("Couldn't establish database connection: %s", err)
--- api/infra/sqlhandler.go
+++ api/infra/sqlhandler.go
@@ -1,7 +1,9 @@
 package infra
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
@@ -12,7 +14,12 @@ type SqlHandler struct {
 }
 
 func NewSqlHandler() *SqlHandler {
-	dsn := "root:hogehoge@tcp(db:3306)/dev?charset=utf8mb4&parseTime=True&loc=Local"
+	user := os.Getenv("MYSQL_USER")
+	pass := os.Getenv("MYSQL_PASSWORD")
+	host := os.Getenv("MYSQL_HOST") // ココ!!
+	dbname := os.Getenv("MYSQL_DATABASE")
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local", user, pass, host, dbname)
+
 	conn, err := gorm.Open("mysql", dsn)
 	if err != nil {
 		log.Fatalf("Couldn't establish database connection: %s", err)
