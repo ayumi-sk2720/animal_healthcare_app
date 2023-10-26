0796e695af8463c7f6ccf4e17c12ad1b97a114d3
myantyuWorld <nepinepi984@gmail.com>
2023-10-26 12:15:16 +0900

    AddSchedule:: v-model指定して、console.log出力

--- vue/src/views/AddSchedule.vue
+++ vue/src/views/AddSchedule.vue
@@ -6,19 +6,42 @@
         <div class="p-4 lg:px-6 bg-white rounded-lg grid justify-center">
             <div class="max-w-7xl p-4">
                 <label for="title" class="block p-2">タイトル</label>
-                <input type="text" id="title" placeholder="例）トリミング" class="block w-full p-2 border border-gray-700 rounded">
+                <input type="text" id="title" placeholder="例）トリミング" class="block w-full p-2 border border-gray-700 rounded" v-model="title">
             </div>
             <div class="max-w-7xl p-4">
                 <label for="datetime-local" class="block p-2">日時</label>
-                <input type="datetime-local" id="datetime-local" class="block w-full p-2 border border-gray-700 rounded">
+                <input type="datetime-local" id="datetime-local" class="block w-full p-2 border border-gray-700 rounded" v-model="date">
             </div>
             <div class="max-w-7xl p-4">
                 <label for="place" class="block p-2">場所</label>
-                <input type="text" id="place" placeholder="例）ペテモ 立川店" class="block w-full p-2 border border-gray-700 rounded">
+                <input type="text" id="place" placeholder="例）ペテモ 立川店" class="block w-full p-2 border border-gray-700 rounded" v-model="basyo">
             </div>
             <div class="p-4 text-center">
-                <button type="submit" class="w-4/5 p-3 bg-orange-500 text-white rounded">作成する</button>
+                <button type="submit" class="w-4/5 p-3 bg-orange-500 text-white rounded" v-on:click="onClickPost">作成する</button>
             </div>
         </div>
     </div>
 </template>
+
+<script>
+import axios from 'axios';
+export default {
+    data(){
+        return{
+            schedule: [],
+            title: '',
+            date: '',
+            basyo: '',
+        }
+    },
+    mounted(){
+    },
+    methods: {
+        onClickPost: function (e) {
+            console.log(this.title) // トリミング
+            console.log(this.date)  // 2023-10-27T12:13
+            console.log(this.basyo) // hogehoge
+        }
+    }
+}
+</script>
