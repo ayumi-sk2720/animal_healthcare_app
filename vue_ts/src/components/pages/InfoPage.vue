5e5287d5058033e75f199b3a2f1d70515b9429b3
myantyuWorld <nepinepi984@gmail.com>
2023-10-30 22:33:05 +0900

    add card components

--- vue_ts/src/components/pages/InfoPage.vue
+++ vue_ts/src/components/pages/InfoPage.vue
@@ -1,8 +1,12 @@
 <!-- Vite + Vue 3 + TypeScript + Tailwind CSS の簡易環境構築 | https://zenn.dev/showy1/articles/c5d1b5d33552be -->
 <script setup lang="ts">
 import TopSection from "@/components/parts/TopSection.vue";
+import CardTile from "@/components/parts/CardTile.vue";
 </script>
 
 <template>
-  <TopSection msg="Custom Message!!!" />
+  <div>
+    <!-- <TopSection msg="Custom Message!!!" /> -->
+    <CardTile title="予定" description="ホゲホゲ" date="2020/10/30" />
+  </div>
 </template>
---
+++ vue_ts/src/components/parts/CardTile.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+const props = defineProps<{
+  title?: string;
+  description?: string;
+  date?: string;
+}>();
+</script>
+
+<template>
+  <div class="p-4 my-4 lg:px-6 bg-blue rounded-lg">
+    <div
+      class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+    >
+      <h5
+        class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+      >
+        {{ title }}
+      </h5>
+      <p class="font-normal text-gray-700 dark:text-gray-400">
+        {{ description }}
+      </p>
+    </div>
+  </div>
+</template>
