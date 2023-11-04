3e3ff5c2714c7ef56c973ac655ae9af19fd3fd64
myantyuWorld <nepinepi984@gmail.com>
2023-11-04 10:07:17 +0900

    add todo comment

--- vue_ts/src/apis/petRepository.ts
+++ vue_ts/src/apis/petRepository.ts
@@ -14,6 +14,8 @@ export interface Pet {
 
 export default class PetRepository {
   // TODO: AxiosPromise<Pet>にならない？
+  // Express+TypeScriptでレスポンスボディに「ストレスなく」型を付ける | https://techblog.istyle.co.jp/archives/8568
+  // [TypeScript] Axiosのtry/catchでの例外オブジェクトを型付けする | https://dev.classmethod.jp/articles/typescript-typing-exception-objects-in-axios-trycatch/
   public getPetSummary(id: number): AxiosPromise<Pet> {
     return repository.get<Pet>(`${resource}/${id}`);
   }
