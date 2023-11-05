4659a6da460061b82bd0d6dd73cc46f9548c1831
myantyuWorld <nepinepi984@gmail.com>
2023-11-05 18:03:48 +0900

    example.spec.tsを実行できるように修正

--- vue_ts/tests/unit/example.spec.ts
+++ vue_ts/tests/unit/example.spec.ts
@@ -1,6 +1,9 @@
 import { shallowMount } from "@vue/test-utils";
 import HelloWorld from "@/components/HelloWorld.vue";
 
+/***
+ * コンポーネントの表示に関する単純なテスト
+ */
 describe("HelloWorld.vue", () => {
   it("renders props.msg when passed", () => {
     const msg = "new message";
